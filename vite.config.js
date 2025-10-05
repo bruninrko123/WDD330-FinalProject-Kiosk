8049abe6719158920d36495e19c346bb50124665
bruninrko123 <balves@byupathway.edu>
2025-10-04 23:33:35 -0300

    fsda

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-    root: '.',
-});
+// export default defineConfig({
+//   root: ".",
+  
+// });
+
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/WDD330-FinalProject-Kiosk/' : '/'
+}));
