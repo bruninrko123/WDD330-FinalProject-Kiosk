ffd4fc7d869fdffec059043aebdf04eaad0dd9b3
bruninrko123 <balves@byupathway.edu>
2025-10-04 22:47:18 -0300

    end of week 5

--- js/main.mjs
+++ js/main.mjs
@@ -1,3 +1,8 @@
+import ExternalServices from "./ExternalServices.mjs";
 import { loadHeaderfooter } from "./utils.mjs";
 
 loadHeaderfooter();
+
+const news = new ExternalServices()
+
+news.callNewsAPI();
