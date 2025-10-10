17723fab0348276d5bbbe7384165d17887a9d511
bruninrko123 <balves@byupathway.edu>
2025-10-09 21:08:02 -0300

    asdf

--- src/js/ServiceList.mjs
+++ src/js/ServiceList.mjs
@@ -7,7 +7,7 @@ function lockersTemplate(locker) {
 
 function foodTemplate(food) {
   return `
-  <a href= '/service-pages/index.html?food=${food.id}'>
+  <a href= '/service_pages/index.html?food=${food.id}'>
   <ul class="food-card">
   
      <img src="${food.image}" alt="${food.name}" width="300" height="300"> 
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path';
 
-// export default defineConfig({
-//   root: ".",
+export default defineConfig({
+  root: "src",
+  base: "./",
+  outDir: '../dist',
+  rollupOptions: {
+    input: {
+      main: resolve(__dirname, 'src/index.html'),
+      about: resolve(__dirname, 'src/about.html'),
+      order: resolve(__dirname, 'src/order/index.html'),
+      service_listing: resolve(__dirname, 'src/service_listing/index.html'),
+      service_pages: resolve(__dirname, 'src/service_pages/index.html'),
+      }
+    }
   
-// });
+});
 
-export default defineConfig(({ command }) => ({
-  // base: command === 'build' ? '/WDD330-FinalProject-Kiosk/' : '/'
-  base: '/'
-}));
+// export default defineConfig(({ command }) => ({
+//   // base: command === 'build' ? '/WDD330-FinalProject-Kiosk/' : '/'
+//   base: '/'
+// }));
