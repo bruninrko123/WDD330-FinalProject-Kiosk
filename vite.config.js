2d76496e4530c669d01e12e044dd9a84b53b77ad
bruninrko123 <balves@byupathway.edu>
2025-10-09 22:34:44 -0300

    asdf

--- vite.config.js
+++ vite.config.js
@@ -2,22 +2,23 @@ import { defineConfig } from "vite";
 import { resolve } from 'path';
 
 export default defineConfig({
-  // root: "src",
-  base: "/WDD330-FinalProject-Kiosk/",
+  root: "src/",
+  // base: "./",
 
   build: {
-    outDir: "../dist",
+    outDir: '../docs',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        about: resolve(__dirname, "src/about.html"),
-        order: resolve(__dirname, "src/order/index.html"),
-        service_listing: resolve(__dirname, "src/service_listing/index.html"),
-        menu: resolve(__dirname, "src/service_listing/menu.html"),
-        service_pages: resolve(__dirname, "src/service_pages/index.html"),
+        main: resolve(__dirname, 'src/index.html'),
+        about: resolve(__dirname, 'src/about.html'),
+        order: resolve(__dirname, 'src/order/index.html'),
+        service_listing: resolve(__dirname, 'src/service_listing/index.html'),
+        menu: resolve(__dirname, 'src/service_listing/menu.html'),
+        service_pages: resolve(__dirname, 'src/service_pages/index.html'),
       },
     },
   },
+  
 });
 
 // export default defineConfig(({ command }) => ({
